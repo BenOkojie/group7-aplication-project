d501c68b746029d9f3790ebbbacdfffd23ecb45d
okreuder <98560336+okreuder@users.noreply.github.com>
2022-02-21 14:25:28 -0500

    Updated the Login Page. Started to create login form.

--- craftables/LoginPage/LoginPage.js
+++ craftables/LoginPage/LoginPage.js
@@ -1,1 +1,69 @@
 import React from "react";
+
+class Form extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+
+        };
+    }
+
+    handleUsernameChange = (event) => {
+        this.setState({ userName: event.target.value });
+    }
+
+    handlePassword = (event) => {
+        this.setState({ password: event.target.value });
+    }
+
+    handleLogin = (event) => {
+        event.preventDefault();
+
+        if (this.state.userName === '') {
+            this.setState({ errorMessage: 'Enter a description' });
+        }
+        if (this.state.password === '') {
+            this.setState({ errorMessage: 'Enter a password' });
+        }
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleLogin}>
+                {this.state.loginErrorMessage && (
+                    <div className="error">
+                        Invalid Username:
+                        {this.state.loginErrorMessage}
+                    </div>
+                    <div className="error">
+                        Invalid Password:
+                        {this.state.loginErrorMessage}
+                    </div>
+                )}
+
+                <div className="field">
+                    <label>Username:</label>
+                    <input 
+                        type='text'
+                        maxLength={50}
+                        value={this.state.userName}
+                        onChange={handleUsernameChange}
+                    />
+                </div>
+                <div className="field">
+                    <label>Password:</label>
+                    <input
+                        type='password'
+                        maxLength={25}
+                        value={this.state.password}
+                        onChange={handlePassword}
+                    />
+                </div>
+                <button type='submit'>Login</button>
+            </form>
+        );
+    }
+}
+
+export default Form;
